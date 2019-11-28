788af1999130ca9d0937aa8286b7e339239e495f
ChenYCL <aa34913@126.com>
2019-11-28 11:24:48 +0800

    update: 面包屑过渡效果

--- src/pages/Crumb/index.tsx
+++ src/pages/Crumb/index.tsx
@@ -1,15 +1,20 @@
 import React, { Fragment, useState } from 'react';
 import { Props } from '../../types';
+import './index.scss';
 
 const Block = (props: Props): JSX.Element => {
     const [modalShow, useModalShow] = useState<boolean>(false);
     const handleSubmit = (e: React.MouseEventHandler): void => {
         useModalShow(true);
     };
-    return modalShow ? (
-        <div onClick={() => useModalShow(!modalShow)}>22</div>
-    ) : (
-        <div onClick={() => useModalShow(!modalShow)}>33</div>
+    return (
+        <div className="wrapper">
+            <div className="content">
+                <div className="line1"></div>
+                <div className="line2"></div>
+                <div className="line3"></div>
+            </div>
+        </div>
     );
 };
 
