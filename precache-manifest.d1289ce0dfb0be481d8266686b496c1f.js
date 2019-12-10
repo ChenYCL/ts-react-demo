f368da64056cdea8f56d59251cc181beaafcdc28
ChenYCL <aa34913@126.com>
2019-12-10 16:51:47 +0000

    deploy [ci skip]

---
+++ precache-manifest.d1289ce0dfb0be481d8266686b496c1f.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "900a873e3a4d9875e463ae6b0ed61937",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1d70d938ea5d32b60b3b",
+    "url": "./static/css/6.5899b58c.chunk.css"
+  },
+  {
+    "revision": "872f97b9c4b5eade9766",
+    "url": "./static/css/Home.05d6f799.chunk.css"
+  },
+  {
+    "revision": "fd9c87526a2980e62e2e",
+    "url": "./static/css/Homepage.94bd407f.chunk.css"
+  },
+  {
+    "revision": "bf2c6f3bd32c0b5dc2fe",
+    "url": "./static/css/Login.692aa9ef.chunk.css"
+  },
+  {
+    "revision": "3dcc751ffc948a71f5fa",
+    "url": "./static/css/Songs.801a9741.chunk.css"
+  },
+  {
+    "revision": "57fb460834f82a4abf7a",
+    "url": "./static/css/main.7bea9139.chunk.css"
+  },
+  {
+    "revision": "1d70d938ea5d32b60b3b",
+    "url": "./static/js/6.86630e17.chunk.js"
+  },
+  {
+    "revision": "18016e26e85293ee1418",
+    "url": "./static/js/7.215115c9.chunk.js"
+  },
+  {
+    "revision": "872f97b9c4b5eade9766",
+    "url": "./static/js/Home.bb32eb73.chunk.js"
+  },
+  {
+    "revision": "fd9c87526a2980e62e2e",
+    "url": "./static/js/Homepage.5abc81f2.chunk.js"
+  },
+  {
+    "revision": "bf2c6f3bd32c0b5dc2fe",
+    "url": "./static/js/Login.96c19e10.chunk.js"
+  },
+  {
+    "revision": "3dcc751ffc948a71f5fa",
+    "url": "./static/js/Songs.dbdd7de0.chunk.js"
+  },
+  {
+    "revision": "57fb460834f82a4abf7a",
+    "url": "./static/js/main.6d1d42fb.chunk.js"
+  },
+  {
+    "revision": "fea9cd2d6ee2152816e6",
+    "url": "./static/js/runtime-main.7f523463.js"
+  },
+  {
+    "revision": "4c880c60e78ade70c197436d2ae6fd64",
+    "url": "./static/media/001-chat.4c880c60.svg"
+  },
+  {
+    "revision": "b8c9f4aae698350259f91ac04265780e",
+    "url": "./static/media/002-email.b8c9f4aa.svg"
+  },
+  {
+    "revision": "7054df2854cef93f5a68501b9164c76d",
+    "url": "./static/media/003-question.7054df28.svg"
+  },
+  {
+    "revision": "6b008082dfdaf10b8267abe55bf87c50",
+    "url": "./static/media/004-smartphone.6b008082.svg"
+  },
+  {
+    "revision": "d3d5088f6491df12be40d840fcc12e2d",
+    "url": "./static/media/facebook.d3d5088f.svg"
+  },
+  {
+    "revision": "148c30bde0a37d10d0833235fa735654",
+    "url": "./static/media/iMac_Perspective_SharonIrving.148c30bd.png"
+  },
+  {
+    "revision": "bb3fda5c6302b48820cdb70cbec5c4c4",
+    "url": "./static/media/instagram.bb3fda5c.svg"
+  },
+  {
+    "revision": "8a6e848a05fa0f33991bf323d28d4762",
+    "url": "./static/media/login.8a6e848a.jpg"
+  },
+  {
+    "revision": "3abf31323a9e5747c7fb5b335b1f3d52",
+    "url": "./static/media/remixicon.3abf3132.svg"
+  },
+  {
+    "revision": "454eaccf06aeb6c6a9d3cb828461cc54",
+    "url": "./static/media/remixicon.454eaccf.woff2"
+  },
+  {
+    "revision": "9ae2ab3fc48826241f04f254b877c6dd",
+    "url": "./static/media/remixicon.9ae2ab3f.woff"
+  },
+  {
+    "revision": "d53d618eb492c6007beb3507b14010e8",
+    "url": "./static/media/remixicon.d53d618e.ttf"
+  },
+  {
+    "revision": "e4e1729baa2a0fedccd57007b0191711",
+    "url": "./static/media/remixicon.e4e1729b.eot"
+  },
+  {
+    "revision": "7a4d9f0fe157437d3258bbc3b785066d",
+    "url": "./static/media/twitter.7a4d9f0f.svg"
+  },
+  {
+    "revision": "ae99d7166572e55aaaa47c67583b95ef",
+    "url": "./static/media/youtube.ae99d716.svg"
+  }
+]);
